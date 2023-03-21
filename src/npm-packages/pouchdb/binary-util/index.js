f6ce01e3ec09537a3dcf45e1eba17214236a08e7
Frank Lemanschik <frank@lemanschik.com>
2023-03-21 10:15:18 +0100

    git subrepo clone https://github.com/pouchdb/binary-util src/npm-packages/pouchdb/binary-util
    subrepo:
      subdir:   "src/npm-packages/pouchdb/binary-util"
      merged:   "0c4e9f5f"
    upstream:
      origin:   "https://github.com/pouchdb/binary-util"
      branch:   "master"
      commit:   "0c4e9f5f"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ src/npm-packages/pouchdb/binary-util/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+//Abstracts constructing a Blob object, so it also works in older
+//browsers that don't support the native Blob constructor. (i.e.
+//old QtWebKit versions, at least).
+function createBlob(parts, properties) {
+  parts = parts || [];
+  properties = properties || {};
+  try {
+    return new Blob(parts, properties);
+  } catch (e) {
+    if (e.name !== "TypeError") {
+      throw e;
+    }
+    var BlobBuilder = global.BlobBuilder ||
+                      global.MSBlobBuilder ||
+                      global.MozBlobBuilder ||
+                      global.WebKitBlobBuilder;
+    var builder = new BlobBuilder();
+    for (var i = 0; i < parts.length; i += 1) {
+      builder.append(parts[i]);
+    }
+    return builder.getBlob(properties.type);
+  }
+}
+
+//Can't find original post, but this is close
+//http://stackoverflow.com/questions/6965107/ (continues on next line)
+//converting-between-strings-and-arraybuffers
+function arrayBufferToBinaryString(buffer) {
+  var binary = "";
+  var bytes = new Uint8Array(buffer);
+  var length = bytes.byteLength;
+  for (var i = 0; i < length; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return binary;
+}
+
+// This used to be called "fixBinary", which wasn't a very evocative name
+// From http://stackoverflow.com/questions/14967647/ (continues on next line)
+// encode-decode-image-with-base64-breaks-image (2013-04-21)
+function binaryStringToArrayBuffer(bin) {
+  var length = bin.length;
+  var buf = new ArrayBuffer(length);
+  var arr = new Uint8Array(buf);
+  for (var i = 0; i < length; i++) {
+    arr[i] = bin.charCodeAt(i);
+  }
+  return buf;
+}
+
+// shim for browsers that don't support it
+function readAsBinaryString(blob, callback) {
+  var reader = new FileReader();
+  var hasBinaryString = typeof reader.readAsBinaryString === 'function';
+  reader.onloadend = function (e) {
+    var result = e.target.result || '';
+    if (hasBinaryString) {
+      return callback(result);
+    }
+    callback(arrayBufferToBinaryString(result));
+  };
+  if (hasBinaryString) {
+    reader.readAsBinaryString(blob);
+  } else {
+    reader.readAsArrayBuffer(blob);
+  }
+}
+
+// simplified API. universal browser support is assumed
+function readAsArrayBuffer(blob, callback) {
+  var reader = new FileReader();
+  reader.onloadend = function (e) {
+    var result = e.target.result || new ArrayBuffer(0);
+    callback(result);
+  };
+  reader.readAsArrayBuffer(blob);
+}
+
+module.exports = {
+  createBlob: createBlob,
+  readAsArrayBuffer: readAsArrayBuffer,
+  readAsBinaryString: readAsBinaryString,
+  binaryStringToArrayBuffer: binaryStringToArrayBuffer,
+  arrayBufferToBinaryString: arrayBufferToBinaryString
+};
+
